4ba07d7557d78af6a9ed0a279f4e8fc54cd0ad5d
miles waugh <miles.m.waugh@gmail.com>
2022-12-05 02:03:43 -0800

    update

--- pagewriter.py
+++ pagewriter.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 from bs4.formatter import HTMLFormatter
 
-A = '<!DOCTYPE html><html><head><link rel="stylesheet" href="styles.css"></head><body>'
+title = 'Mushoku Tensei'
+
+A = '<!DOCTYPE html><html><head><link rel="stylesheet" href="styles.css"><title>' + \
+    title+'</title></head><body><h1 class="header">'+title+'</h1>'
 
 for C in range(87):
     A += '<img src="images/img'+str(C+1)+'.jpg" class="image">'
