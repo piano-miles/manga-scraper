d85acc25dbfc807a6fe7c0a46c4c82aa0c430be1
miles waugh <miles.m.waugh@gmail.com>
2022-12-05 12:24:41 -0800

    Added support for kissmanga

--- scrape.js
+++ scrape.js
@@ -1,7 +1,11 @@
 // Start
 sessionStorage.clear()
 
+//
 // For each page
+//
+
+// mangaclash.com
 images = [];
 next_page = document.getElementsByClassName("btn next_page")[0];
 
@@ -10,6 +14,26 @@ for (var i = 0; i < 100; i++) {
     if (img != null) {
         images.push(img.getAttribute("src"));
     }
+}
+
+page = 1;
+if (sessionStorage.getItem("page_number") != null) {
+    page = parseInt(sessionStorage.getItem("page_number")) + 1;
+}
+
+sessionStorage.setItem("page_number", page);
+sessionStorage.setItem("page" + page + "_data", images);
+
+next_page.click();
+
+// kissmanga
+images = [];
+next_page = document.getElementById("btnNext");
+
+b = document.getElementById("centerDivVideo");
+c = b.querySelectorAll("img");
+for (var i=0; i<b.childElementCount-1; i++) {
+    images.push(c[i].getAttribute('src'));
 }
 
 page = 1;
